3a2bcbe8ea7323b80eb9682205e3a8751a49149c
Leonan Marques <leonan.marques886@gmail.com>
2025-06-01 11:43:17 -0300

    Add AuthenticationService and its test suite

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsersModule } from "./modules/users/users.module";
+import { AuthenticationService } from "./modules/authentication/authentication.service";
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthenticationService],
 })
 export class AppModule {}
