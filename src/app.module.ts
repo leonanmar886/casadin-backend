dc47773246cde3438d9b8afc3b17d2f6f2252c4d
Leonan Marques <leonan.marques886@gmail.com>
2025-06-08 21:16:40 -0300

    feat(#1): integrate TypeORM for user management and enhance module structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,33 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsersModule } from "./modules/users/users.module";
 import { AuthenticationService } from "./modules/authentication/authentication.service";
+import { AuthenticationModule } from "./modules/authentication/authentication.module";
+import { AuthenticationController } from "./modules/authentication/authentication.controller";
+import { UsersController } from "./modules/users/users.controller";
+import { UsersService } from "./modules/users/users.service";
+import { User } from "./modules/users/models/user.entity";
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService, AuthenticationService],
+  imports: [UsersModule, AuthenticationModule],
+  controllers: [AppController, AuthenticationController, UsersController],
+  providers: [AppService, AuthenticationService, UsersService],
+})
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "casadin",
+      password: "casadin",
+      database: "casadin_db",
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
